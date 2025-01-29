0d9859d956c48d7139784db8818e413f223a306f
Zerefez <202209849@post.au.dk>
2025-01-29 15:33:16 +0100

    LECTion 2 debugg

---
+++ Lection-2/MauiTodo/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿
+
+using MauiTodo.Models;
+
+namespace MauiTodo
+{
+	public partial class MainPage : ContentPage
+	{
+		string _todoListData = string.Empty; // Values of the to-do items
+		readonly Database _database; // Stores an instance of the database class
+
+		public MainPage(Database database)
+		{
+			InitializeComponent();
+			_database = new Database(); //create an instance of the database class & assign it to the _database field.
+			_ = Initialize(); //Uses the discard variable to call our Initialize method
+		}
+
+		private async Task Initialize()
+		{
+			// Initialization logic here
+			await Task.CompletedTask;
+		}
+
+		private async void Button_Clicked(object sender, EventArgs e)
+		{
+			// Button click logic here
+
+			// Create a new TodoItem object
+			var todoItem = new TodoItem
+			{
+				Title = _todoListData,
+				Done = false,
+				Due = DateTime.Now,
+			};
+
+			todoItem.Id = await _database.Addtodo(todoItem); // Add the new TodoItem to the database
+			_todoListData = string.Empty; // Clear the _todoListData field
+			await Initialize(); // Reinitialize the page
+		}
+	}
+
+}
+
+
