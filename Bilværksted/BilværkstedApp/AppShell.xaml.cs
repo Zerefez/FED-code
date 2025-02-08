aacd2543ecd626c87443617cebe188f65ca6f487
Zerefez <202209849@post.au.dk>
2025-02-08 13:05:57 +0100

    Bilværksted START

---
+++ Bilværksted/BilværkstedApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BilværkstedApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Bilværksted/BilværkstedApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BilværkstedApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
